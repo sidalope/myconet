df233cb472b782adfd545ddee11f67fa5736a915
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2023-09-04 11:09:37 +0200

    feat: remove txt recording and fix date format

--- record_scd4x_data_i2c.c
+++ record_scd4x_data_i2c.c
@@ -69,83 +69,72 @@ int main(void) {
                error);
     }
 
-	
-	time_t rawtime;
-	struct tm *info;
-	time(&rawtime);
-	info = localtime(&rawtime);
-	char buffer[80];
-	strftime(buffer, 80, "%d-%m-%Y %H:%M:%S", info);
-	printf("Start at: %s \n", buffer);
-	
-	char text_data_file[80];
-	char csv_data_file[80];
-	strftime(text_data_file, 80, "sensor_data_%d-%m-%Y %H:%M:%S.txt", info);
-	strftime(csv_data_file, 80, "sensor_data_%d-%m-%Y %H:%M:%S.csv", info);
-	
-	// Initialize csv file
-	FILE *fdata = fopen(csv_data_file, "a");
-	if (fdata == NULL)
-	{
-		printf("Error opening the file %s", csv_data_file);
-		return -1;
-	}
-	fprintf(fdata, "time, co2(ppm), temperature(C), rel_humidity(percent)\n");
-	fclose(fdata);
 
-    printf("Waiting for first measurement... (5 sec)\n");		
-		
+    time_t rawtime;
+    struct tm *info;
+    time(&rawtime);
+    info = localtime(&rawtime);
+    char buffer[80];
+    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", info);
+    printf("Start at: %s \n", buffer);
+
+    char csv_data_file[80];
+    strftime(csv_data_file, 80, "sensor_data_%Y-%m-%d %H:%M:%S.csv", info);
+
+    // Initialise csv file
+    FILE *fdata = fopen(csv_data_file, "a");
+    if (fdata == NULL)
+    {
+        printf("Error opening the file %s", csv_data_file);
+        return -1;
+    }
+    fprintf(fdata, "time, co2(ppm), temperature(°C), rel_humidity(percent)\n");
+    fclose(fdata);
+
+    printf("Waiting for first measurement... (5 sec)\n");
+
     for (;;) {
-		// Open files for writing
-		FILE *ftext = fopen(text_data_file, "a");
-		if (ftext == NULL)
-		{
-			printf("Error opening the file %s", text_data_file);
-			return -1;
-		}
-		FILE *fdata = fopen(csv_data_file, "a");
-		if (fdata == NULL)
-		{
-			printf("Error opening the file %s", csv_data_file);
-			return -1;
-		}
-		
-		
-		// Get current time and read and write measurement
+        // Open file for writing
+        FILE *fdata = fopen(csv_data_file, "a");
+        if (fdata == NULL)
+        {
+            printf("Error opening the file %s", csv_data_file);
+            return -1;
+        }
+
+
+        // Get current time and read and write measurement
         sensirion_i2c_hal_sleep_usec(5000000);
         
         time_t rawtime;
-		struct tm *info;
-		time(&rawtime);
-		info = localtime(&rawtime);
-		char buffer[80];
-		strftime(buffer, 80, "%d-%m-%Y %H:%M:%S", info);
-		
+        struct tm *info;
+        time(&rawtime);
+        info = localtime(&rawtime);
+        char date[80];
+        strftime(date, 80, "%Y-%m-%d %H:%M:%S", info);
+
         uint16_t co2;
         float temperature;
         float humidity;
         error = scd4x_read_measurement(&co2, &temperature, &humidity);
         if (error) {
             printf("Error executing scd4x_read_measurement(): %i\n", error);
-            fprintf(ftext, "Error executing scd4x_read_measurement(): %i\n", error);
+            fprintf(fdata, "Error executing scd4x_read_measurement(): %i\n", error);
         } else if (co2 == 0) {
             printf("Invalid sample detected, skipping.\n");
-            fprintf(ftext, "Invalid sample detected, skipping.\n");
+            fprintf(fdata, "Invalid sample detected, skipping.\n");
         } else {
-			printf("%s\n", buffer);
-            printf("CO2: %u ppm\n", co2);
-            printf("Temperature: %.2f °C\n", temperature);
-            printf("Humidity: %.2f RH\n\n", humidity);
-            fprintf(ftext, "%s\n", buffer);
-            fprintf(ftext, "CO2: %u ppm\n", co2);
-            fprintf(ftext, "Temperature: %.2f °C\n", temperature);
-            fprintf(ftext, "Humidity: %.2f RH\n\n", humidity);
-            fprintf(fdata, "%s, %u, %.2f, %.2f\n", buffer, co2, temperature, humidity);
+            //printf("%s\n", date);
+            //printf("CO2: %u ppm\n", co2);
+            //printf("Temperature: %.2f °C\n", temperature);
+            //printf("Humidity: %.2f RH\n\n", humidity);
+            printf("%s, %u, %.2f, %.2f\n", date, co2, temperature, humidity);
+            fprintf(fdata, "%s, %u, %.2f, %.2f\n", date, co2, temperature, humidity);
         }
-		fclose(ftext);
-		fclose(fdata);
+        fclose(ftext);
+        fclose(fdata);
     }
-	
-	
+
+
     return 0;
 }
